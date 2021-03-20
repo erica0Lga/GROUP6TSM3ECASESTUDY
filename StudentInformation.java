b0c0849bb56b547df72a55d187cd0969233b543e
MattFlordeliza <mr.flordeliza1070@student.tsu.edu.ph>
2021-03-20 22:30:58 +0800

    Methods5and6

--- StudentInformation.java
+++ StudentInformation.java
@@ -4,7 +4,10 @@ public class StudentInformation
 	private String Mname;
 	private String Lname;
 	private String bDate;
+	private String add;
+	private String gen;
 	
+	 //Method 1: Manalo Olga Erica
   public void getFname(String Fname)
     {
         this.Fname = Fname;
@@ -14,7 +17,7 @@ public class StudentInformation
     {
         return Fname;
     }
-    
+     //Method 2: Manalo Olga Erica
     public void getMname(String Mname)
     {
         this.Mname = Mname;
@@ -24,6 +27,7 @@ public class StudentInformation
     {
         return Mname;
     }
+     //Method 3: Fajardo Dynel Angelo
 	public void getLname(String Lname)
     {
         this.Lname = Lname;
@@ -33,6 +37,7 @@ public class StudentInformation
     {
         return Lname;
     }
+     //Method 4:Fajardo Dynel Angelo
     public void getBDate(String bDate)
     {
         this.bDate = bDate;
@@ -40,5 +45,26 @@ public class StudentInformation
 	public String showBDate()
     {
         return bDate;
+    }
+    //Method 5: Flordeliza Matt Reuben
+    public void getAddress(String add)
+    {
+        this.add = add;
+    }
+ 
+    public String showAddress()
+    {
+        return add;
+    }
+    
+    //Method 6: Flordeliza Matt Reuben 
+    public void getGender(String gen)
+    {
+        this.gen = gen;
+    }
+ 
+    public String showGender()
+    {
+        return gen;
     }
 }
